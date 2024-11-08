6e9af13e4a9b67ecf98c1faa32066b58ed4e1c7c
LimShiHui <83006580+LimShiHui@users.noreply.github.com>
2024-11-08 13:00:28 +0800

    Merging into Main: SH - html for user admin (#2)
    * upload sample code for login
    
    * login page and create account page
    
    * GUI update
    
    Admin:
    - home page
    - view account list (search
    - create account
    - view account
    - edit account
    
    * GUI update
    
    logout page
    
    * GUI update
    
    User admin - profile
    
    * html edit
    
    * organise files and update references
    
    ---------
    
    Co-authored-by: Yier3 <133016840+yier3an@users.noreply.github.com>

---
+++ website/boundaries/loginUI.js
@@ -0,0 +1,50 @@
+// boundaries/loginUI.js
+import loginController from "../controllers/loginController.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("login_btn").addEventListener("click", () => {
+		const email = document.getElementById("username").value;
+		const password = document.getElementById("password").value;
+		loginController(email, password);
+	});
+});
+
+// note to Seth: have to use class too!! see if you can combine,
+// if not I can try for these too (Chloe)
+/*
+class loginUI {
+	constructor() {
+		username = document.forms["login"]["username"].value;
+		pwd = document.forms["login"]["password"].value;
+		loginC = new loginController(username, pwd);
+	}
+
+	displayLoginPg() {
+	//
+	}
+
+	attemptLogin() {
+		// validate the login
+		let loginStatus = loginC.validateLogin(loginC.user);
+
+		if (loginStatus) {
+			this.loginSuccess();
+		}
+		else {
+			this.loginError();
+		}
+	}
+
+	loginSuccess() {
+		// redirect to user home page
+		// window.location.replace("/home.html");
+		console.log("login success");
+	}
+
+	loginError() {
+		// show error, ask to try again
+		// alert("Entered invalid username or password. Please try again.");
+		console.log("login error");
+	}
+}
+*/
