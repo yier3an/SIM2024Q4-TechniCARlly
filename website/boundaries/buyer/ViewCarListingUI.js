80620221e2bcc03703894b1d30b0158d792a9bd5
Yier3 <133016840+yier3an@users.noreply.github.com>
2024-11-15 22:17:47 +0800

    update

--- website/boundaries/buyer/ViewCarListingUI.js
+++ website/boundaries/buyer/ViewCarListingUI.js
@@ -18,19 +18,47 @@ class ViewCarListingUI {
 	}
 
 	async displayListing() {
-		const listing = document.getElementsByClassName("car");
+		const listing = document.getElementById("listingContainer");
+		var i = 0;
 
-		// clear table body
-		listing.innerHTML = "";
-
-		for (item in this.carList) {
-			let row = document.createElement("tr");
-
-			let clName = document.createElement("td");
-			clName.textContent = item.listingName;
-			row.appendChild(clName);
-
-			listing.appendChild(row);
+		if (!Array.isArray(listing) || !listing.length) {
+			document.getElementById("displayError").style.display = "block";
 		}
+		else {
+			document.getElementById("displayError").style.display = "none";
+
+			if (listing.innerHTML == "" || listing.innerHTML == null) {
+				for (item in this.carList) {
+
+					let image = document.createElement("img");
+					if (i > 6) {
+						i = 0;
+					}
+					image.setAttribute("src", "../../static/asset/" + i + ".jpg");
+					listing.appendChild(image);
+					i++;
+
+					let div1 = document.createElement("div");
+					div1.setAttribute("class", "car_content");
+
+					let table = document.createElement("table");
+					table.setAttribute("id", "listing_table");
+
+					let row = document.createElement("tr");
+					let clName = document.createElement("td");
+					clName.textContent = "Listing Name: " + item.listingName;
+					row.appendChild(clName);
+					table.appendChild(row);
+
+					div1.appendChild(table);
+
+					listing.appendChild(div1);
+				}
+			}
+			else {
+				listing.innerHTML = "";
+			}
+		}
+
 	}
 }
